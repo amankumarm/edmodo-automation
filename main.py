04474fa0d97042d765b82d7f86b86bae58aeaabf
amankumarm <amankumarm441@gmail.com>
2021-11-04 15:15:46 +0530

    deploying3

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import json
 def get_assignments():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.binary_location = "./binary/chrome-95/chromedriver" # remove this is production
+    # chrome_options.binary_location = "./binary/chrome-95/chromedriver" # remove this is production
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
